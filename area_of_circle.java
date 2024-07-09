b344336534636524871a2937172eff8fd83db6c3
Minhazul Abdin Emon <165420980+MinhazulAbdinEmon@users.noreply.github.com>
2024-07-09 20:50:17 +0600

    Add files via upload
    The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
    
    Calculate the area using the formula given in the problem description.
    
    Input
    The input contains a value of floating point (double precision), that is the variable R.
    
    Output
    Present the message "A=" followed by the value of the variable, as in the example bellow, with four places after the decimal point. Use all double precision variables. Like all the problems, don't forget to print the end of line after the result, otherwise you will receive "Presentation Error".
    
    Input Samples	| Output Samples
    2.00
    
    A=12.5664
    
    100.64
    
    A=31819.3103
    
    150.00
    
    A=70685.7750

---
+++ area_of_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble();
+        // float pie = 3.14159f;
+        // float area = pie * float_num * float_num;
+        System.out.printf("A=%.4f\n", (3.14159*num*num));
+
+    }
+}
